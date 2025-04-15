54efbed1dc87e9066265953983fa3bd28a1bc193
Michaell0128 <karyan02@naver.com>
2025-04-16 07:04:42 +0900

    Update woldo_automation_final_complete.py

--- woldo_automation_final_complete.py
+++ woldo_automation_final_complete.py
@@ -67,8 +67,6 @@ with tabs[0]:
         a_df = pd.read_excel(a_file)
         if not a_df.empty and 0 in a_df.index:
             a_df = a_df.drop(index=0)
-        if not a_df.empty and 0 in a_df.index:
-            a_df = a_df.drop(index=0)
         b_df = pd.read_excel(b_file)
         st.session_state.pending_matches.clear()
         st.session_state.selected_matches.clear()
